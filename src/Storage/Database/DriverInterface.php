0cd6dd358bd42ecc9b36c06279387f0c5151de6e
Jonatan Fransson <jonte.frasse@gmail.com>
2024-03-30 09:07:28 +0100

    Added uses + ws cleanup

--- src/Storage/Database/DriverInterface.php
+++ src/Storage/Database/DriverInterface.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Projom\Storage\Database;
 
+use Projom\Storage\Database\Drivers;
 use Projom\Storage\Database\QueryInterface;
 use Projom\Storage\Database\Query\Field;
 use Projom\Storage\Database\Query\Collection;
 use Projom\Storage\Database\Query\Filter;
+use Projom\Storage\Database\SourceInterface;
 
-interface DriverInterface 
-{	
+interface DriverInterface
+{
 	public function type(): Drivers;
 	public function execute(string $sql, ?array $params): mixed;
 	public function select(Collection $table, Field $field, Filter $filter): mixed;
