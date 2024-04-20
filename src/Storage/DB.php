d1341b4811b3984c4344c1192ece231dabaae694
Jonatan Fransson <jonte.frasse@gmail.com>
2024-04-20 09:25:49 +0200

    Changed type hint of params parameter

--- src/Storage/DB.php
+++ src/Storage/DB.php
@@ -7,14 +7,14 @@ namespace Projom\Storage;
 use Projom\Storage\Database\Engine;
 use Projom\Storage\Database\Query;
 
-class DB extends Engine 
+class DB extends Engine
 {
 	public static function query(string $table): Query
 	{
 		return static::dispatch($table);
 	}
 
-	public static function sql(string $query, ?array $params = null): mixed
+	public static function sql(string $query, array|null $params = null): mixed
 	{
 		return static::dispatch($query, $params);
 	}
