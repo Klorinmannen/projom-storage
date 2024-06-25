cf9b4795b5b393589d4ae292fe8565a095984736
Jonatan Fransson <jonte.frasse@gmail.com>
2024-06-25 13:01:36 +0200

    Removed use on php exception class

--- src/Storage/Database/Engine/DSN.php
+++ src/Storage/Database/Engine/DSN.php
@@ -4,19 +4,18 @@ declare(strict_types=1);
 
 namespace Projom\Storage\Database\Engine;
 
-use Exception;
 use Projom\Storage\Database\Engine\Config;
 
 class DSN
 {
-    public static function MySQL(Config $config): string
-    {    
+	public static function MySQL(Config $config): string
+	{
 		if (!$host = $config->host)
-			throw new Exception('Missing DNS server host', 400);
+			throw new \Exception('Missing DNS server host', 400);
 		if (!$port = $config->port)
-			throw new Exception('Missing DNS server port', 400);
+			throw new \Exception('Missing DNS server port', 400);
 		if (!$database = $config->database)
-			throw new Exception('Missing DNS database name', 400);
+			throw new \Exception('Missing DNS database name', 400);
 
 		$parts = [
 			"host=$host",
@@ -31,5 +30,5 @@ class DSN
 			$parts[] = "collation=$collation";
 
 		return 'mysql:' . implode(';', $parts);
-    }
+	}
 }
