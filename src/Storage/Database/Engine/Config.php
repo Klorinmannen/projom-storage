10f65b6b3f7e2538aea9f31b1320df6f09d67c02
Jonatan Fransson <jonte.frasse@gmail.com>
2024-06-26 22:08:51 +0200

    Refactored default values

--- src/Storage/Database/Engine/Config.php
+++ src/Storage/Database/Engine/Config.php
@@ -4,28 +4,30 @@ declare(strict_types=1);
 
 namespace Projom\Storage\Database\Engine;
 
+use Projom\Storage\Database\Engine\Driver;
+
 class Config
 {
-	public string $host;
-	public string $port;
-	public string $username;
-	public string $password;
-	public string $database;
-	public string $driver;
-	public string $charset;
-	public string $collation;
+	public string $host = '';
+	public string $port = '';
+	public string $username = '';
+	public string $password = '';
+	public string $database = '';
+	public Driver|null $driver = null;
+	public string $charset = '';
+	public string $collation = '';
 	public array $options = [];
 
 	public function __construct(array $config) 
 	{
-		$this->host = $config['host'] ?? null;
-		$this->port = $config['port'] ?? null;
-		$this->username = $config['username'] ?? null;
-		$this->password = $config['password'] ?? null;
-		$this->database = $config['database'] ?? null;
-		$this->driver = $config['driver'] ?? null;
-		$this->charset = $config['charset'] ?? null;
-		$this->collation = $config['collation'] ?? null;
+		$this->host = $config['host'] ?? '';
+		$this->port = $config['port'] ?? '';
+		$this->username = $config['username'] ?? '';
+		$this->password = $config['password'] ?? '';
+		$this->database = $config['database'] ?? '';
+		$this->driver = Driver::tryFrom($config['driver'] ?? '');
+		$this->charset = $config['charset'] ?? '';
+		$this->collation = $config['collation'] ?? '';
 		$this->options = $config['options'] ?? [];
 	}
 }
