ec70e7f0afe0c66ff7a660f3ededa29927ab2c1d
Jonatan Fransson <jonte.frasse@gmail.com>
2024-08-07 13:37:21 +0200

    Removed transaction methods

--- src/Storage/Database/Engine/DriverInterface.php
+++ src/Storage/Database/Engine/DriverInterface.php
@@ -9,7 +9,4 @@ use Projom\Storage\Database\Query\Action;
 interface DriverInterface
 {
 	public function dispatch(Action $action, mixed $args): mixed;
-	public function startTransaction(): void;
-	public function endTransaction(): void;
-	public function revertTransaction(): void;
 }
