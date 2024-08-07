49042e359e5234c12036e763aa1497f156065417
Jonatan Fransson <jonte.frasse@gmail.com>
2024-08-07 13:37:54 +0200

    Moved enum positions

--- src/Storage/Database/Query/Action.php
+++ src/Storage/Database/Query/Action.php
@@ -6,12 +6,12 @@ namespace Projom\Storage\Database\Query;
 
 enum Action
 {
-	case EXECUTE;
-	case QUERY;
 	case SELECT;
 	case INSERT;
 	case UPDATE;
 	case DELETE;
+	case EXECUTE;
+	case QUERY;
 	case COUNT;
 	case SUM;
 	case AVG;
