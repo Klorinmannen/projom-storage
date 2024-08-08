60a478dcccb46fa73d4402b3c9bd25ed9f2e389e
Jonatan Fransson <jonte.frasse@gmail.com>
2024-08-08 14:01:37 +0200

    Reordered methods

--- src/Storage/Database/Engine.php
+++ src/Storage/Database/Engine.php
@@ -17,18 +17,24 @@ class Engine
 	protected static Driver|null $currentDriver = null;
 	protected static DriverFactory|null $driverFactory = null;
 
+	public static function start(): void
+	{
+		$sourceFactory = SourceFactory::create();
+		$driverFactory = DriverFactory::create($sourceFactory);
+		static::setDriverFactory($driverFactory);
+	}
+
+	public static function clear(): void
+	{
+		static::$drivers = [];
+		static::$currentDriver = null;
+		static::$driverFactory = null;
+	}
+
 	public static function dispatch(Action $action, mixed $args = null): mixed
 	{
 		$driver = static::driver();
 		return $driver->dispatch($action, $args);
-	}
-
-	public static function driver(): DriverInterface
-	{
-		$driver = static::$drivers[static::$currentDriver?->value] ?? null;
-		if ($driver === null)
-			throw new \Exception("Engine driver not set", 400);
-		return $driver;
 	}
 
 	public static function useDriver(Driver $driver): void
@@ -48,28 +54,22 @@ class Engine
 		static::setDriver($engineDriver, $config->driver);
 	}
 
+	public static function driver(): DriverInterface
+	{
+		$driver = static::$drivers[static::$currentDriver?->value] ?? null;
+		if ($driver === null)
+			throw new \Exception("Engine driver not set", 400);
+		return $driver;
+	}
+
 	public static function setDriver(DriverInterface $engineDriver, Driver $driver): void
 	{
 		static::$drivers[$driver->value] = $engineDriver;
 		static::$currentDriver = $driver;
 	}
 
-	public static function start(): void
-	{
-		$sourceFactory = SourceFactory::create();
-		$driverFactory = DriverFactory::create($sourceFactory);
-		static::setDriverFactory($driverFactory);
-	}
-
 	public static function setDriverFactory(DriverFactory $driverFactory): void
 	{
 		static::$driverFactory = $driverFactory;
-	}
-
-	public static function clear(): void
-	{
-		static::$drivers = [];
-		static::$currentDriver = null;
-		static::$driverFactory = null;
 	}
 }
