262e4e0acb53a40f42d6447319a89327af6ee610
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2024-08-17 09:49:54 +0200

    None existing configurations are now default nullable

--- src/Storage/Database/Engine/Config.php
+++ src/Storage/Database/Engine/Config.php
@@ -8,28 +8,28 @@ use Projom\Storage\Database\Engine\Driver;
 
 class Config
 {
-	public readonly Driver|null $driver;
-	public readonly string $username;
-	public readonly string $password;
-	public readonly string $host;
-	public readonly string|int $port;
-	public readonly string $database;
-	public readonly string $charset;
-	public readonly string $collation;
-	public readonly string $dsn;
+	public readonly null|Driver $driver;
+	public readonly null|string $username;
+	public readonly null|string $password;
+	public readonly null|string $host;
+	public readonly null|string|int $port;
+	public readonly null|string $database;
+	public readonly null|string $charset;
+	public readonly null|string $collation;
+	public readonly null|string $dsn;
 	public readonly array $options;
 
 	public function __construct(array $config)
 	{
 		$this->driver = Driver::tryFrom($config['driver'] ?? '');
-		$this->username = $config['username'] ?? '';
-		$this->password = $config['password'] ?? '';
-		$this->host = $config['host'] ?? '';
-		$this->port = $config['port'] ?? '';
-		$this->database = $config['database'] ?? '';
-		$this->charset = $config['charset'] ?? '';
-		$this->collation = $config['collation'] ?? '';
-		$this->dsn = $config['dsn'] ?? '';
+		$this->username = $config['username'] ?? null;
+		$this->password = $config['password'] ?? null;
+		$this->host = $config['host'] ?? null;
+		$this->port = $config['port'] ?? null;
+		$this->database = $config['database'] ?? null;
+		$this->charset = $config['charset'] ?? null;
+		$this->collation = $config['collation'] ?? null;
+		$this->dsn = $config['dsn'] ?? null;
 		$this->options = $config['options'] ?? [];
 	}
 }
