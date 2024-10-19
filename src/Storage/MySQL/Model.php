c8562511f27fe48c40d4c845a8d07ad8318abcbc
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2024-10-19 13:55:26 +0200

    More work done on MySQL\Model functionality

--- src/Storage/MySQL/Model.php
+++ src/Storage/MySQL/Model.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Projom\Storage\MySQL;
 
 use Projom\Storage\MySQL;
+use Projom\Storage\SQL\Util\Filter;
 use Projom\Storage\SQL\Util\Operator;
+use Projom\Storage\Util;
 
 class Model
 {
@@ -25,85 +27,175 @@ class Model
 		static::$primaryField = static::$class::PRIMARY_FIELD;
 	}
 
-	protected static function find(string|int $primaryID): array
+	/**
+	 * Find a record.
+	 * 
+	 * * Example use: User::find($userID = 3)
+	 */
+	public static function find(string|int $primaryID): null|array
 	{
 		static::invoke();
 
 		$records = MySQL::query(static::$class)->fetch(static::$primaryField, $primaryID);
+		if (!$records)
+			return null;
 
-		return $records;
+		return array_pop($records);
 	}
 
-	protected static function all(): array
+	/**
+	 * Get all records.
+	 * 
+	 * * Example use: User::all()
+	 */
+	public static function all(): null|array
 	{
 		static::invoke();
 
 		$records = MySQL::query(static::$class)->select();
+		if (!$records)
+			return null;
 
-		return $records;
+		$keydRecords = Util::rekey($records, static::$primaryField);
+
+		return $keydRecords;
 	}
 
-	protected static function get(string $field, mixed $value): array
+	/**
+	 * Get a record.
+	 * 
+	 * * Example use: User::get('Email', 'John.doe@example.com')
+	 */
+	public static function get(string $field, mixed $value): null|array
 	{
 		static::invoke();
 
 		$records = MySQL::query(static::$class)->fetch($field, $value);
+		if (!$records)
+			return null;
 
-		return $records;
+		$keydRecords = Util::rekey($records, static::$primaryField);
+
+		return $keydRecords;
 	}
 
-	protected static function update(string|int $primaryID, array $data): void
+	/**
+	 * Update a record.
+	 * 
+	 * * Example use: User::update($userID = 3, ['Name' => 'A new name'])
+	 */
+	public static function update(string|int $primaryID, array $data): int
 	{
 		static::invoke();
 
-		MySQL::query(static::$class)->filterOn(static::$primaryField, $primaryID)->update($data);
+		$rowsAffected = MySQL::query(static::$class)->filterOn(static::$primaryField, $primaryID)->update($data);
+
+		return $rowsAffected;
 	}
 
-	protected static function delete(string|int $primaryID): void
+	/**
+	 * Delete a record.
+	 * 
+	 * * Example use: User::delete($userID = 3)
+	 */
+	public static function delete(string|int $primaryID): void
 	{
 		static::invoke();
 
 		MySQL::query(static::$class)->filterOn(static::$primaryField, $primaryID)->delete();
 	}
 
-	protected static function create(array $record): void
+	/**
+	 * Delete all records.
+	 * 
+	 * * Example use: User::deleteAll()
+	 */
+	public static function deleteAll(): void
 	{
 		static::invoke();
 
-		MySQL::query(static::$class)->insert($record);
+		MySQL::query(static::$class)->delete();
 	}
 
-	protected static function search(string $field, mixed $value): array
+	/**
+	 * Create a record.
+	 * 
+	 * * Example use: User::create(['Name' => 'John'])
+	 */
+	public static function create(array $record): int|string
+	{
+		static::invoke();
+
+		$primaryID = MySQL::query(static::$class)->insert($record);
+
+		return $primaryID;
+	}
+
+	/**
+	 * Search for records.
+	 * 
+	 * * Example use: User::search('Name', '%John%')
+	 */
+	public static function search(string $field, mixed $value): null|array
 	{
 		static::invoke();
 
 		$records = MySQL::query(static::$class)->filterOn($field, $value, Operator::LIKE)->select();
+		if (!$records)
+			return null;
 
-		return $records;
+		$keydRecords = Util::rekey($records, static::$primaryField);
+
+		return $keydRecords;
 	}
 
-	protected static function clone(string|int $primaryID): array
+	/**
+	 * Clone a record.
+	 * 
+	 * * Example use: User::clone($userID = 3)
+	 * * Example use: User::clone($userID = 3, ['Name' => 'New Name'])
+	 */
+	public static function clone(string|int $primaryID, array $newRecord = []): array
 	{
 		static::invoke();
 
-		$record = MySQL::query(static::$class)->fetch(static::$primaryField, $primaryID);
+		$records = MySQL::query(static::$class)->fetch(static::$primaryField, $primaryID);
+		if (!$records)
+			return throw new \Exception('Record to clone not found', 400);
+
+		$record = array_pop($records);
 		unset($record[static::$primaryField]);
 
-		$id = MySQL::query(static::$class)->insert($record);
-		$record = MySQL::query(static::$class)->fetch(static::$primaryField, $id);
+		foreach ($newRecord as $field => $value)
+			if (array_key_exists($field, $record))
+				$record[$field] = $value;
 
-		return $record;
+		$clonePrimaryID = MySQL::query(static::$class)->insert($record);
+		$clonedRecords = MySQL::query(static::$class)->fetch(static::$primaryField, $clonePrimaryID);
+
+		return array_pop($clonedRecords);
 	}
 
-	public static function count(string $field, mixed $value): int
+	/**
+	 * Count the number of records.
+	 * 
+	 * * Example use: User::count()
+	 * * Example use: User::count($filter)
+	 */
+	public static function count(null|array $filter = null): int
 	{
 		static::invoke();
 
-		$records = MySQL::query(static::$class)->filterOn($field, $value)->select('COUNT(*) as count');
-		$record = array_pop($records);
-		if ($record === null)
+		$records = [];
+		if ($filter !== null)
+			$records = MySQL::query(static::$class)->filterOnGroup([$filter])->select('COUNT(*) as count');
+		else
+			$records = MySQL::query(static::$class)->select('COUNT(*) as count');
+
+		if (!$records)
 			return 0;
 
+		$record = array_pop($records);
 		return (int) $record['count'];
 	}
 }
