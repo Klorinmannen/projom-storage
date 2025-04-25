9132b31b74ce5105f1af8a96a923b27fc57b8056
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-04-25 12:57:28 +0200

    User class for test

---
+++ tests/EndToEnd/Static/User.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Projom\Tests\EndToEnd\Static;
+
+use Projom\Storage\Static\MySQL\Repository;
+
+class User
+{
+	use Repository;
+
+	public static function primaryField(): string
+	{
+		return 'UserID';
+	}
+
+	public static function redactFields(): array
+	{
+		return [
+			'Password'
+		];
+	}
+
+	public static function formatFields(): array
+	{
+		return [
+			'UserID' => 'int',
+			'Firstname' => 'string',
+			'Lastname' => 'string',
+			'Username ' => 'string',
+			'Password' => 'string',
+			'Active' => 'bool',
+			'Created' => 'date',
+			'Updated' => 'datetime'
+		];
+	}
+}
