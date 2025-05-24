cb95a92613426389b92223fb9e29f5ab4332ead2
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-05-24 12:37:05 +0200

    Added new test cases

--- tests/Unit/Storage/MySQL/UtilTest.php
+++ tests/Unit/Storage/MySQL/UtilTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Projom\Tests\Unit\Storage\MySQL;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
@@ -11,12 +12,39 @@ use Projom\Storage\MySQL\Util;
 
 class UtilTest extends TestCase
 {
+	public static function dynamicTableNameWithNamespaceProvider(): array
+	{
+		return [
+			'correct use namespace name' => [true, 'App\Recipe\Ingredient\Repository', 'RecipeIngredient'],
+			'malformed use namespace name' => [true, 'App\Recipe\IngredientRepository', 'Recipe'],
+			'correct class name 1' => [false, 'App\Recipe\IngredientRepository', 'Ingredient'],
+			'correct class name 2' => [false, 'App\Recipe\Ingredient', 'Ingredient'],
+			'malformed class name' => [false, 'App\Recipe\Ingredient\Repository', ''],
+		];
+	}
+
+	#[Test]
+	#[DataProvider('dynamicTableNameWithNamespaceProvider')]
+	public function dynamicTableNameWithNamespace(bool $useNamespaceAsTableName, string $calledClass, string $expected): void
+	{
+		$table = Util::dynamicTableName($calledClass, $useNamespaceAsTableName);
+		$this->assertEquals($expected, $table);
+	}
+
 	#[Test]
 	public function tableFromCalledClass(): void
 	{
-		$calledClass = 'Projom\Storage\MySQL\Util';
+		$calledClass = 'App\Recipe\IngredientRepository';
 		$class = Util::tableFromCalledClass($calledClass);
-		$this->assertEquals('Util', $class);
+		$this->assertEquals('Ingredient', $class);
+	}
+
+	#[Test]
+	public function tableFromNamespace(): void
+	{
+		$calledClass = 'App\Recipe\Ingredient\Repository';
+		$table = Util::tableFromNamespace($calledClass);
+		$this->assertEquals('RecipeIngredient', $table);
 	}
 
 	#[Test]
