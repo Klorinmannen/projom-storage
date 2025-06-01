7d64b5f19ae2513dc47a9a0da14e5f016a255c71
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2025-06-01 22:37:40 +0200

    Added read and write CSV methods

--- src/Storage/Engine/Driver/Connection/Util.php
+++ src/Storage/Engine/Driver/Connection/Util.php
@@ -18,4 +18,41 @@ class Util extends StorageUtil
 			return false;
 		return true;
 	}
+
+	public static function readCSV(
+		string $filePath,
+		string $delimiter,
+		string $enclosure,
+		string $escape
+	): array {
+
+		if (! static::fileExists($filePath))
+			throw new \Exception("CSV file: $filePath, is not readable.", 400);
+
+		$fileData = [];
+
+		$file = fopen($filePath, 'r');
+		while ($line = fgetcsv($file, null, $delimiter, $enclosure, $escape))
+			$fileData[] = $line;
+		fclose($file);
+
+		return $fileData;
+	}
+
+	public static function writeCSV(
+		string $filePath,
+		array $fileData,
+		string $delimiter,
+		string $enclosure,
+		string $escape
+	): void {
+
+		if (! static::fileExists($filePath))
+			throw new \Exception("CSV file: $filePath, is not writable.", 400);
+
+		$file = fopen($filePath, 'w');
+		foreach ($fileData as $line)
+			fputcsv($file, $line, $delimiter, $enclosure, $escape);
+		fclose($file);
+	}
 }
