87641c212e95c3b5acb84d8fa9e15c869d26dcd3
Jonatan Rao-Fransson <jonte.frasse@gmail.com>
2026-02-12 20:02:11 +0100

    Moved query ns from Facade to Database

---
+++ src/Storage/Database/Query.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JRF\Storage\Database;
+
+use JRF\Storage\Engine\Driver\Driver;
+use JRF\Storage\Query\Action;
+use JRF\Storage\Query\Util;
+use JRF\Storage\SQL\Statement\Builder;
+use JRF\Storage\Internal\Registry;
+
+class Query
+{
+	public static function build(string|array $collections, array $options = []): Builder
+	{
+		$collections = Util::stringToArray($collections);
+		$manager = Registry::get();
+		return $manager->dispatch(Action::QUERY, Driver::MySQL, [$collections, $options]);
+	}
+
+	public static function sql(string $sql, null|array $params = null): mixed
+	{
+		$manager = Registry::get();
+		return $manager->dispatch(Action::EXECUTE, Driver::MySQL, [$sql, $params]);
+	}
+
+	public static function useConnection(int|string $name): void
+	{
+		$manager = Registry::get();
+		$manager->dispatch(Action::CHANGE_CONNECTION, Driver::MySQL, $name);
+	}
+
+	public static function startTransaction(): void
+	{
+		$manager = Registry::get();
+		$manager->dispatch(Action::START_TRANSACTION, Driver::MySQL);
+	}
+
+	public static function endTransaction(): void
+	{
+		$manager = Registry::get();
+		$manager->dispatch(Action::END_TRANSACTION, Driver::MySQL);
+	}
+
+	public static function revertTransaction(): void
+	{
+		$manager = Registry::get();
+		$manager->dispatch(Action::REVERT_TRANSACTION, Driver::MySQL);
+	}
+}
